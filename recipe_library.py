8fcca6ba709fd3fdecc610c120721aab201473ce
Chris Yang <chris@dominodatalab.com>
2018-10-14 14:21:41 -0700

    more changes

--- recipe_library.py
+++ recipe_library.py
@@ -2,5 +2,10 @@
 
 
 class RecipeLibrary:
+    def add_recipe(self, recipe):
+        # TODO
+        pass
+
     def get_recipes(self):
+        # un-hard-code eventually obviously
         return [whiskey_sour, manhattan, negroni]
--- test_ui.py
+++ test_ui.py
@@ -1,7 +1,30 @@
 import readline
 
+from recipe_library import RecipeLibrary
+from recipe import Recipe
 
-recipe = readline("What recipe?...")
+
+# Hi jason!
+
+def print_recipes(library):
+    for recipe in library.get_recipes():
+
+
+# https://www.bonappetit.com/recipe/whiskey-sour
+whiskey_sour = Recipe(
+    {
+        "bourbon": 2,
+        "lemon_juice": 0.75,
+        "simple_syrup": 0.75
+    }
+)
+
+library = RecipeLibrary()
+
+print_recipes(library)
+
+
+recipe = readline.("What recipe?...")
 
 recipe = RecipeLibrary().look_up_recipe_by_name("whiskey_sour")
 
