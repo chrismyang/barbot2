ddd2937cff106309479069281d85ed8db79381e7
Chris Yang <chris@dominodatalab.com>
2019-02-09 15:28:40 -0800

    (1) Made changes to pin numbers to add all the new pumps (2) Add selection menu for Arduino USB port path

--- test_ui.py
+++ test_ui.py
@@ -7,6 +7,7 @@ from ingredient_layout import IngredientLayout
 from pump_config import PumpConfig
 from recipe_library import RecipeLibrary
 from recipe import Recipe
+import serial.tools.list_ports
 
 # https://www.bonappetit.com/recipe/whiskey-sour
 whiskey_sour = Recipe("Whiskey Sour", {
@@ -17,18 +18,35 @@ whiskey_sour = Recipe("Whiskey Sour", {
 
 library = RecipeLibrary(whiskey_sour)
 
+
+ports = list(serial.tools.list_ports.comports())
+print("Select the port that the Arduino is connected on")
+print("=" * 80)
+for i in range(len(ports)):
+    port = ports[i]
+    print(f"({i}) {port.device} {port.description} {port.manufacturer}")
+
+selection = int(input("Type in a number from the choices above: "))
+
+# arduino_device_path = "/dev/cu.usbmodem14111"
+
+arduino_device_path = ports[selection].device
+
 driver = SimpleArduinoDriver(
-    "/dev/tty.usbmodem14411",
+    arduino_device_path,
     [
-        PumpConfig("pump_a", enable_pin_number=4, forward_pin_number=2, reverse_pin_number=3),
-        PumpConfig("pump_b", enable_pin_number=13, forward_pin_number=11, reverse_pin_number=12)
+        PumpConfig("P1", enable_pin_number=12, forward_pin_number=51, reverse_pin_number=50),
+        PumpConfig("P2", enable_pin_number=10, forward_pin_number=49, reverse_pin_number=48),
+        PumpConfig("P3", enable_pin_number=8, forward_pin_number=44, reverse_pin_number=45),
+        PumpConfig("P4", enable_pin_number=7, forward_pin_number=43, reverse_pin_number=42),
+        PumpConfig("P5", enable_pin_number=9, forward_pin_number=47, reverse_pin_number=46)
     ],
     flow_rate=0.2213762726 # oz/sec
 )
 
 ingredient_layout = IngredientLayout({
-    "pump_a": "bourbon",
-    "pump_b": "lemon_juice",
+    "P2": "bourbon",
+    "P3": "lemon_juice",
     "pump_c": "simple_syrup"
 })
 
@@ -52,14 +70,26 @@ if __name__ == "__main__":
 
         driver.close()
     elif command == "purge":
-        pump_to_purge = sys.argv[2]
+        pump_to_prime = sys.argv[2]
 
         input(f'Hit enter to begin purge of pump {pump_to_purge}:')
         print(f"Purging pump '{pump_to_purge}'...")
-        driver.start_purge(pump_to_purge)
+        driver.start_purge(pump_to_prime)
 
         input('Hit enter to stop purge')
-        driver.end_purge(pump_to_purge)
+        driver.end_purge(pump_to_prime)
+
+        driver.close()
+
+    elif command == "prime":
+        pump_to_prime = sys.argv[2]
+
+        input(f'Hit enter to begin prime of pump {pump_to_prime}:')
+        print(f"Prime pump '{pump_to_prime}'...")
+        driver.start_prime(pump_to_prime)
+
+        input('Hit enter to stop prime')
+        driver.end_prime(pump_to_prime)
 
         driver.close()
 
